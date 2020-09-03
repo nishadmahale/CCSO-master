f162cda4dc0bb06560162c1a7ec54e779630abfb
nishadmahale <70054272+nishadmahale@users.noreply.github.com>
2020-09-03 17:07:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var car,wall;
+var speed,weight;
+
+
+
+
+car.velocityX=speed;
+
+function setup () {
+
+    car=createSprite(50,200,50,50);
+    wall=createSprite(1500,200,60,height/2);
+    
+    car.velocityX=speed;
+    speed=random(55,90);
+    weight=random(400,1500);
+
+
+
+
+}
+
+
+function draw(){
+    
+    if(wall.x-car.x<(car.width+wall.width)/2) {
+        
+        damage=0.5*weight*speed*speed/22500;
+        car.velocityX=0;
+    if(damage<100){
+            car.shapeColour="Green";
+       }
+    if (damage > 100 && damage <180){
+       car.shapeColour="Yellow";
+      }
+    if(damage>180){
+       car.shapeColour="Red";
+   }
+    }
+    
+
+drawSprites();
+}
